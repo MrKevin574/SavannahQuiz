4878bfae7dcc38d1f258297efe4d0e394f26278e
MrKevin574 <mrkevin574@gmail.com>
2022-06-27 14:51:21 -0400

    Changing Colors Palette

--- app/src/main/java/com/mrkevin574/quizsabana/ui/theme/Color.kt
+++ app/src/main/java/com/mrkevin574/quizsabana/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.mrkevin574.quizsabana.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val PrimaryColor = Color(0xffafb42b)
+val PrimaryVariant= Color(0xffcddc39)
+val SecondaryColor = Color(0xff9e9e9e)
--- app/src/main/java/com/mrkevin574/quizsabana/ui/theme/Theme.kt
+++ app/src/main/java/com/mrkevin574/quizsabana/ui/theme/Theme.kt
@@ -1,42 +1,25 @@
 package com.mrkevin574.quizsabana.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val AppColors = lightColors(
+    primary = PrimaryColor,
+    primaryVariant = PrimaryVariant,
+    secondary = SecondaryColor
 )
 
 @Composable
-fun QuizSabanaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun QuizSabanaTheme(content: @Composable () -> Unit) {
+
+    val systemUIController = rememberSystemUiController()
+
+    systemUIController.setNavigationBarColor(PrimaryColor, false)
 
     MaterialTheme(
-        colors = colors,
+        colors = AppColors,
         typography = Typography,
         shapes = Shapes,
         content = content
